4bb8bc7fd26563a578ade03f56aaa0cc4948c9dd
bryanndev <bryansoares012@hotmail.com>
2022-12-15 01:31:26 -0300

    Working

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var lista = ("", "", "", "", "", "", "")
+
+
+
+//var nome = "Bryan Soares";
+//var idade = 21;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace ("Japão", "Brasil"));
+   
