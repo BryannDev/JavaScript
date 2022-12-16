da77e7951b92230de3087394647729bdc8bedda6
bryanndev <bryansoares012@hotmail.com>
2022-12-15 22:33:16 -0300

    aula 5

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-var lista = ("", "", "", "", "", "", "")
+function soma(n1, n2){
+    return n1 + n2;
+}
 
+function validaIdade(Idade) {
+  var validar;
+  if (Idade >= 18){
+    validar = true
+  }else{
+    validar = false
+  }
+  return validar;
+}
 
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
 
+//alert(soma(5, 10));
+//var lista = ("", "", "", "", "", "", "");
 //var nome = "Bryan Soares";
 //var idade = 21;
 //var idade2 = 10;
